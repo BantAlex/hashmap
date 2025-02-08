4876908a7f724e4a3d7f35f08a2e1b2080c3fed3
BantAlex <bantalex3@gmail.com>
2025-02-08 09:42:45 +0200

    Hashmap complete

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,13 +1,14 @@
 class HashMap
-  attr_writer :load_factor, :capacity
+  # attr_writer :load_factor, :capacity, :length, :buckets, :index
 
   def initialize
-    self.load_factor = nil
-    self.capacity = 16
-    self.length
+    @load_factor = nil
+    @capacity = 16
+    @buckets = Array.new(@capacity) {[]}
+    @length = 0
   end
 
-  def array_restriction
+  def array_restriction(index)
     raise IndexError if index.negative? || index >= @buckets.length
   end
 
@@ -22,38 +23,99 @@ class HashMap
 
   def set(key,value)
     hashed = self.hash(key)
+    index = hashed % @capacity
+    array_restriction(index)
 
+    if @buckets[index].nil?
+      new_bucket = []
+      new_bucket << [key,value]
+    else
+      bucket = @buckets[index]
+      bucket << [key,value]
+    end
+
+    @length += 1
   end
 
   def get(key)
-
+    hashed = self.hash(key)
+    index = hashed % @capacity
+    array_restriction(index)
+    bucket = @buckets[index]
+    return nil if bucket.nil?
+    bucket.each do |pair|
+      return pair[1] if pair[0] == key
+    end
   end
 
   def has?(key)
-
+    return true if get(key)
+    return false
   end
 
   def remove(key)
+    hashed = self.hash(key)
+    index = hashed % @capacity
+    array_restriction(index)
+    bucket = @buckets[index]
+    return nil if bucket.nil?
+    @buck_index = nil
+    @buck_value = nil
 
+    bucket.each_with_index do |pair, i|
+      if pair[0] == key
+        @buck_index = i
+        @buck_value = pair[1]
+        break
+      end
+    end
+    if @buck_index
+      bucket.delete_at(@buck_index)
+      @length -= 1
+      return @buck_value
+    else
+      return nil
+    end
   end
 
   def length
-
+    @length
   end
 
   def clear
-
+    initialize
   end
 
   def keys
-
+    keys_arr = []
+    @buckets.each do |bucket|
+      next if bucket.nil?
+      bucket.each do |pair|
+        keys_arr << pair[0]
+      end
+    end
+    keys_arr
   end
 
   def values
-
+    values_arr = []
+    @buckets.each do |bucket|
+      next if bucket.nil?
+      bucket.each do |pair|
+        values_arr << pair[1]
+      end
+    end
+    values_arr
   end
 
   def entries
-
+    entries_arr = []
+    @buckets.each do |bucket|
+      next if bucket.nil?
+      bucket.each do |pair|
+        entries_arr << pair
+      end
+    end
+    entries_arr
   end
 end
--- main.rb
+++ main.rb
@@ -2,3 +2,4 @@ require_relative 'lib/hash_map'
 require_relative 'lib/hash_set'
 
 hash_map = HashMap.new #the key is what the hash function will take as an input.
+p hash_map.hash("bitch")
